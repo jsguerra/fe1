8694f6b19f5962ac6f623d020f9b718c9735a341
jsguerra <jose@gcostudios.com>
2023-03-14 09:19:36 +0000

    added webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+    (config.experiments = { layers: true, topLevelAwait: true }),
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "main",
+          remotes: {
+            blog: `blog@http://localhost:3001/_next/static/${
+              isServer ? "ssr" : "chunks"
+            }/remoteEntry.js`,
+          },
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./header": "./components/Header.jsx",
+          },
+        })
+      );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
